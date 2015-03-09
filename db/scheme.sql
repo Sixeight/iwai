6ac5f36bfa56bbc1940481652ecdf9be7946b664
Tomohiro Nishimura <tomohiro68@gmail.com>
2015-03-09 17:18:53 +0900

    Update scheme a bit

--- db/scheme.sql
+++ db/scheme.sql
@@ -1,12 +1,13 @@
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
   id         SERIAL PRIMARY KEY,
   name       VARCHAR(32) NOT NULL UNIQUE,
   twitter_id INT NOT NULL UNIQUE
 );
+/* CREATE INDEX has no IF NOT EXISTS */
 CREATE INDEX users_twitter_id_index ON users (twitter_id);
 
-CREATE TABLE wishlists (
+CREATE TABLE IF NOT EXISTS wishlists (
   id    SERIAL PRIMARY KEY,
   url   VARCHAR(64) NOT NULL UNIQUE,
   title VARCHAR(64) NOT NULL,
