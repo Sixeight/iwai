cb92f941db95695a17d85e40cb6f5b17f48c11b0
Tomohiro Nishimura <tomohiro68@gmail.com>
2015-03-10 03:49:04 +0900

    Format scheme

--- db/scheme.sql
+++ db/scheme.sql
@@ -3,17 +3,19 @@ CREATE TABLE IF NOT EXISTS users (
   id         SERIAL PRIMARY KEY,
   name       VARCHAR(32) NOT NULL UNIQUE,
   twitter_id INT NOT NULL UNIQUE,
+
   joined_at TIMESTAMP NOT NULL
 );
 /* CREATE INDEX has no IF NOT EXISTS */
 CREATE INDEX users_twitter_id_index ON users (twitter_id);
 
 CREATE TABLE IF NOT EXISTS wishlists (
-  id    SERIAL PRIMARY KEY,
-  url   VARCHAR(64) NOT NULL UNIQUE,
-  title VARCHAR(64) NOT NULL,
-  memo  VARCHAR(255),
+  id      SERIAL PRIMARY KEY,
+  url     VARCHAR(64) NOT NULL UNIQUE,
+  title   VARCHAR(64) NOT NULL,
+  memo    VARCHAR(255),
   user_id INT NOT NULL,
+
   created_at TIMESTAMP NOT NULL,
-  updatd_at TIMESTAMP NOT NULL
+  updatd_at  TIMESTAMP NOT NULL
 );
