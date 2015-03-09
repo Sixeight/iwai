a049a07038daf738cdfac4927cc11af330c8b653
Tomohiro Nishimura <tomohiro68@gmail.com>
2015-03-10 02:29:33 +0900

    Add timestamps to schme

--- db/scheme.sql
+++ db/scheme.sql
@@ -2,7 +2,8 @@
 CREATE TABLE IF NOT EXISTS users (
   id         SERIAL PRIMARY KEY,
   name       VARCHAR(32) NOT NULL UNIQUE,
-  twitter_id INT NOT NULL UNIQUE
+  twitter_id INT NOT NULL UNIQUE,
+  joined_at TIMESTAMP NOT NULL
 );
 /* CREATE INDEX has no IF NOT EXISTS */
 CREATE INDEX users_twitter_id_index ON users (twitter_id);
@@ -11,5 +12,7 @@ CREATE TABLE IF NOT EXISTS wishlists (
   id    SERIAL PRIMARY KEY,
   url   VARCHAR(64) NOT NULL UNIQUE,
   title VARCHAR(64) NOT NULL,
-  memo  VARCHAR(255)
+  memo  VARCHAR(255),
+  created_at TIMESTAMP NOT NULL,
+  updatd_at TIMESTAMP NOT NULL
 );
