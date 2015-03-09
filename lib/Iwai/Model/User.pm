933f1bc194e5ade0358b092386e5df3c9928ea51
Tomohiro Nishimura <tomohiro68@gmail.com>
2015-03-10 04:15:25 +0900

    Implement models

---
+++ lib/Iwai/Model/User.pm
@@ -0,0 +1,41 @@
+package Iwai::Model::User;
+
+use strict;
+use warnings;
+
+# CREATE TABLE IF NOT EXISTS users (
+#   id         SERIAL PRIMARY KEY,
+#   name       VARCHAR(32) NOT NULL UNIQUE,
+#   twitter_id INT NOT NULL UNIQUE,
+#   joined_at TIMESTAMP NOT NULL
+# );
+# /* CREATE INDEX has no IF NOT EXISTS */
+# CREATE INDEX users_twitter_id_index ON users (twitter_id);
+
+use Class::Accessor::Lite::Lazy (
+  ro      => [qw( id name twitter_id )],
+  ro_lazy => [qw( joined_at )],
+  new     => 1
+);
+
+use Iwai::Util;
+
+sub _build_joined_at {
+  my $self = shift;
+  $self->{_joined_at} ||= eval {
+    Iwai::Util->time_from_string($self->{joined_at});
+  };
+}
+
+sub to_hash_ref {
+  my $self = shift;
+  {
+    id        => $self->id,
+    name      => $self->name,
+    joined_at => $self->joined_at,
+  }
+}
+
+1;
+
+__END__
---
+++ lib/Iwai/Model/Wishlist.pm
@@ -0,0 +1,52 @@
+package Iwai::Model::Wishlist;
+
+use strict;
+use warnings;
+
+# CREATE TABLE IF NOT EXISTS wishlists (
+#   id    SERIAL PRIMARY KEY,
+#   url   VARCHAR(64) NOT NULL UNIQUE,
+#   title VARCHAR(64) NOT NULL,
+#   memo  VARCHAR(255),
+#   created_at TIMESTAMP NOT NULL,
+#   updatd_at TIMESTAMP NOT NULL
+# );
+# /* CREATE INDEX has no IF NOT EXISTS */
+# CREATE INDEX wishlists_user_id_index ON wishlists (user_id);
+
+
+use Class::Accessor::Lite::Lazy (
+  ro      => [qw( id url title memo )],
+  ro_lazy => [qw( created_at updated_at )],
+  new     => 1,
+);
+
+use Iwai::Util;
+
+sub _build_created_at {
+  my $self = shift;
+  $self->{_created_at} ||= eval {
+    Iwai::Util->time_from_string($self->{created_at});
+  };
+}
+
+sub _build_updated_at {
+  my $self = shift;
+  $self->{_updated_at} ||= eval {
+    Iwai::Util->time_from_string($self->{updated_at});
+  };
+}
+
+sub to_hash_ref {
+  my $self = shift;
+  {
+    id         => $self->id,
+    url        => $self->url,
+    title      => $self->title,
+    memo       => $self->memo,
+    created_at => $self->{created_at},
+    updated_at => $self->{updated_at},
+  }
+}
+
+1;
