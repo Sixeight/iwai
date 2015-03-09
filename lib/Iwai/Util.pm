03bdd301bb12e487e134e25b01827a57b0702f5d
Tomohiro Nishimura <tomohiro68@gmail.com>
2015-03-10 04:14:47 +0900

    Add utilities for datetime handling

---
+++ lib/Iwai/Util.pm
@@ -0,0 +1,32 @@
+package Iwai::Util;
+
+use strict;
+use warnings;
+
+use DateTime;
+use DateTime::Format::Pg;
+
+use Iwai::Config;
+
+sub now {
+  my $now = DateTime->now(time_zone => config->param("db")->{time_zone});
+  $now->set_formatter(DateTime::Format::Pg->new);
+  $now;
+}
+
+sub time_from_string {
+  my ($class, $string) = @_;
+  my $dt = DateTime::Format::Pg->parse_datetime($string);
+  $dt->set_time_zone(config->param("db")->{time_zone});
+  $dt->set_formatter(DateTime::Format::Pg->new);
+  $dt;
+}
+
+sub time_to_string {
+  my ($class, $time) = @_;
+  $time->set_time_zone(config->param("db")->{time_zone});
+  $time->set_formatter(DateTime::Format::Pg->new);
+  "" . $time;
+}
+
+1;
