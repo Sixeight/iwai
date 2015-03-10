e2fe8f5c98ee57e25418e59192c749a2afe6ef72
Tomohiro Nishimura <tomohiro68@gmail.com>
2015-03-11 01:02:58 +0900

    Add minify pipe to browserify task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require("gulp");
 var browserify = require("browserify");
 var source = require("vinyl-source-stream");
+var buffer = require("vinyl-buffer");
 var reactify = require("reactify");
 var watch = require("gulp-watch");
 var plumber = require("gulp-plumber");
+var uglify = require('gulp-uglify');
 
 gulp.task("browserify", function() {
   var bs = browserify({
@@ -15,12 +17,20 @@ gulp.task("browserify", function() {
     .pipe(plumber())
     .pipe(bs.bundle())
     .pipe(source("app.js"))
+    .pipe(buffer())
+    .pipe(uglify())
     .pipe(gulp.dest("./public/js"));
 });
 
 gulp.task("watch", function() {
   gulp.watch("src/*.js",  ["browserify"]);
   gulp.watch("src/*.jsx", ["browserify"]);
+});
+
+gulp.task('compress', function() {
+  gulp.src('lib/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'))
 });
 
 gulp.task("default", ["browserify"]);
