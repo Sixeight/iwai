44676fb11e88c9dd3572ba955dea73f380abd0ef
Tomohiro Nishimura <tomohiro68@gmail.com>
2015-03-11 00:51:53 +0900

    Implement endpoint to create wishlist

--- lib/Iwai/Controller/List.pm
+++ lib/Iwai/Controller/List.pm
@@ -3,6 +3,10 @@ package Iwai::Controller::List;
 use strict;
 use warnings;
 
+use Iwai::Service::Wishlist;
+use Iwai::Util::ListInfoFetcher;
+use Iwai::Error;
+
 sub index {
   my ($class, $c) = @_;
   $c->render_text("index");
@@ -16,7 +20,25 @@ sub json {
 
 sub create {
   my ($class, $c) = @_;
-  $c->render_text("add");
+  my $params = $c->request->parameters;
+  my $url = $params->{url};
+  my $info = Iwai::Util::ListInfoFetcher->fetch($url)
+    or die Iwai::Error->new(code => 404);
+
+  my $title = $info->{title};
+  my $name  = $info->{name};
+  my $birth = $info->{birth};
+  my $desc  = $info->{desc};
+  $title = [split(":", $title)]->[-1];
+  Iwai::Service::Wishlist->create({
+    url     => $url,
+    title   => $title,
+    name    => $name,
+    birth   => $birth,
+    desc    => $desc,
+    user_id => $c->user->id,
+  });
+  $c->render_text("ok");
 }
 
 sub update {
