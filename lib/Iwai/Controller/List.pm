fc85598e6074449e6bb2ad4e61770aed00acd300
Tomohiro Nishimura <tomohiro68@gmail.com>
2015-03-11 14:04:15 +0900

    Update controller to follow relation table scheme

--- lib/Iwai/Controller/List.pm
+++ lib/Iwai/Controller/List.pm
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 
 use Iwai::Service::Wishlist;
+use Iwai::Service::UserWishlist;
 use Iwai::Util::ListInfoFetcher;
 use Iwai::Error;
 
@@ -14,7 +15,7 @@ sub index {
 
 sub json {
   my ($class, $c) = @_;
-  my $lists = Iwai::Service::Wishlist->find_all_by_user_id($c->user->id);
+  my $lists = Iwai::Service::UserWishlist->all_wishlists_by_user_id($c->user->id);
   $c->render_json([map { $_->to_hash_ref } @$lists]);
 }
 
@@ -23,6 +24,32 @@ sub create {
   my $params = $c->request->parameters;
   my $url = $params->{url};
   $url =~ s/^https/http/;
+
+  my $wishlist = Iwai::Service::Wishlist->find_by_url($url);
+  my $list_id;
+  if ($wishlist) {
+    $list_id = $wishlist->id;
+  } else {
+    $class->create_wish_list($url);
+    $list_id = Iwai::Service::Wishlist->last_insert_id;
+  }
+  Iwai::Service::UserWishlist->create($c->user->id, $list_id);
+  $c->render_text("ok");
+}
+
+sub update {
+  my ($class, $c) = @_;
+  $c->render_text("update");
+}
+
+sub delete {
+  my ($class, $c) = @_;
+  $c->render_text("remove");
+}
+
+sub create_wish_list {
+  my ($class, $url) = @_;
+
   my $info = Iwai::Util::ListInfoFetcher->fetch($url)
     or die Iwai::Error->new(code => 404);
 
@@ -37,19 +64,7 @@ sub create {
     name    => $name,
     birth   => $birth,
     desc    => $desc,
-    user_id => $c->user->id,
   });
-  $c->render_text("ok");
-}
-
-sub update {
-  my ($class, $c) = @_;
-  $c->render_text("update");
-}
-
-sub delete {
-  my ($class, $c) = @_;
-  $c->render_text("remove");
 }
 
 1;
