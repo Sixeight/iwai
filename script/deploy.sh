922327c0d5d6b1dee31b76ca1524f37d8e061327
Tomohiro Nishimura <tomohiro68@gmail.com>
2015-03-11 22:23:08 +0900

    Add deploy script

---
+++ script/deploy.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+git reset HEAD --hard
+git checkout deploy
+git merge master
+if [ ! -f config/database.yml ]; then
+  echo "create database.yml"
+  cp config/{_,}database.yml
+fi
+if [ ! -f public/js/app.js ]; then
+  gulp browserify
+fi
+git commit -am "deploy commit"
+git push heroku deploy:master
