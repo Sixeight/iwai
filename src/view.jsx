5cd2b1c7933501481db73b395b718fde982ac94b
Tomohiro Nishimura <tomohiro68@gmail.com>
2015-03-11 16:37:17 +0900

    Refactor around Store callbacks

--- src/view.jsx
+++ src/view.jsx
@@ -14,49 +14,53 @@ var App = React.createClass({
     };
   },
   componentDidMount: function() {
-    Store.on("fetch", function(list) {
-      this.setState({
-        wishlists: list,
-      });
-    }.bind(this));
-    Store.on("error", function(errorMsg) {
-      var list = this.state.wishlists;
-      for (var i = 0; i < list.length; i++) {
-        if (list[i].title == "読み込み中") {
-          list.splice(i, 1);
-          break;
-        }
+    Store.on("fetch", this.fetchCalback);
+    Store.on("error", this.errorCallback);
+    Store.on("change", this.changeCallback);
+    Store.on("adding", this.addingCallback);
+    Store.on("removing", this.removingCallback);
+    Store.fetchAll();
+  },
+  fetchCalback: function(list) {
+    this.setState({
+      wishlists: list,
+    });
+  },
+  errorCallback: function(errorMsg) {
+    var list = this.state.wishlists;
+    for (var i = 0; i < list.length; i++) {
+      if (list[i].title == "読み込み中") {
+        list.splice(i, 1);
+        break;
       }
+    }
+    this.setState({
+      wishlists: list,
+      error: errorMsg
+    });
+    setTimeout(function() {
       this.setState({
-        wishlists: list,
-        error: errorMsg
+        error: ""
       });
-      setTimeout(function() {
-        this.setState({
-          error: ""
-        });
-      }.bind(this), 3000);
-    }.bind(this));
-    var updateList = function() {
-      Store.fetchAll();
-    };
-    Store.on("change", updateList);
-    Store.on("adding", function() {
-      var list = this.state.wishlists;
-      list.push({url: "#", title: "読み込み中"});
-      this.setState({
-        wishlists: list,
-      });
-    }.bind(this));
-    Store.on("removing", function(id) {
-      var list = this.state.wishlists.filter(function(list) {
-        return list.id != id
-      });
-      this.setState({
-        wishlists: list
-      });
-    }.bind(this));
-    updateList();
+    }.bind(this), 3000);
+  },
+  changeCallback: function() {
+    Store.fetchAll();
+  },
+  addingCallback: function() {
+    var list = this.state.wishlists;
+    list.push({url: "#", title: "読み込み中"});
+    this.setState({
+      wishlists: list,
+    });
+  },
+  removingCallback: function(id) {
+    var list = this.state.wishlists.filter(function(list) {
+      return list.id != id
+    });
+    this.setState({
+      wishlists: list
+    });
   },
   handleSearch: function(word) {
     this.setState({
